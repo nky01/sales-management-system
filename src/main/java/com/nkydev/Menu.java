f06a73f7ec81ebfef6483822250465a2311b9fe7
Nicole Cayo <nickybelu@gmail.com>
2026-06-06 12:24:39 -0300

    Implement menu navigation loop

--- src/main/java/com/nkydev/Menu.java
+++ src/main/java/com/nkydev/Menu.java
@@ -6,24 +6,32 @@ public class Menu {
 
     public void showMenu() {
         Scanner keyword = new Scanner(System.in);
-        System.out.println("\n===== Sales Management System =====\n");
-        System.out.println("1. Create customer");
-        System.out.println("2. Create product");
-        System.out.println("3. Create order");
-        System.out.println("4. Add product to the order");
-        System.out.println("5. Show orders");
-        System.out.println("6. Show products");
-        System.out.println("7. Exit");
+        int option = 0;
 
-        System.out.print("\nSelect an option: ");
-        int option = keyword.nextInt();
-
+        while (option != 8) {
+            System.out.println("\n===== Sales Management System =====\n");
+            System.out.println("1. Create customer");
+            System.out.println("2. Show customers");
+            System.out.println("3. Create product");
+            System.out.println("4. Show products");
+            System.out.println("5. Create order");
+            System.out.println("6. Show orders");
+            System.out.println("7. Add product to order");
+            System.out.println("8. Exit");
+            System.out.println("===================================");
+            System.out.print("\nSelect an option: ");
+            option = keyword.nextInt();
+        }
         switch (option) {
-            case 1 -> System.out.println();
+            case 1 -> createCustomer();
             case 2 -> System.out.println();
             case 3 -> System.out.println();
-            case 4 -> System.out.println();
+            case 8 -> System.out.println("Thank you & goodbye!");
             default -> System.out.println("Incorrect option. Try again...");
         }
+    }
+
+    public void createCustomer() {
+        System.out.println("Create customer");
     }
 }
